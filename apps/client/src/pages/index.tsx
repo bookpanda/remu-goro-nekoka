5ea5da12813d2906fba3443cdd3d1cd260f38d69
Idhibhat Pankam <marginpankam@gmail.com>
2023-02-23 23:30:12 +0700

    setup video

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,24 +1,24 @@
-import { useUserCountQuery } from "@org/codegen";
-import { Button } from "@org/design";
-
 import { MyPage } from "$core/@types";
 
 const IndexPage: MyPage = () => {
-  const { data } = useUserCountQuery();
-
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-4 p-8 text-center transition-colors">
-      <h1 className="text-6xl font-bold text-blue-600 dark:text-blue-500">
-        Turbo Next Nest GraphQL Prisma
-      </h1>
-
-      <p className="text-4xl font-bold">My personal stack for production app</p>
-
-      <p>Testing Apollo =&gt; User Count = {data?.userCount}</p>
-
-      <a href="https://github.com/Leomotors/turbo-next-nest-graphql-prisma">
-        <Button>GitHub</Button>
-      </a>
+    <main className="h-screen w-screen bg-red-100">
+      <video
+        className="video-js"
+        controls
+        data-setup="{}"
+        id="my-player"
+        poster="//vjs.zencdn.net/v/oceans.png"
+        preload="auto"
+      >
+        <track
+          kind="captions"
+          label="english_captions"
+          src="goro.mp4"
+          srcLang="en"
+        />
+        <source src="/goro.mp4" type="video/mp4"></source>
+      </video>
     </main>
   );
 };
